78d582d339d3d9941fc534275905231a17da01dc
albecannoni <alberto.cannoni@outlook.it>
2022-12-13 10:38:40 +0100

    update buldingPage

--- assets/JS/mainScript.js
+++ assets/JS/mainScript.js
@@ -1,13 +1,12 @@
-//?funzione che genera la Bulding page
 window.addEventListener('load', init)
 
-
-
-function init()
-{
+function init() {
     buildingPage();
 };
+
+//***funzione che genera la Bulding page
 function buildingPage() {
+    let body = document.querySelector('#Body')
     let container = document.querySelector('#container');
     let navbar = document.querySelector('#navbar')
     navbar.innerHTML = '';
@@ -15,4 +14,5 @@ function buildingPage() {
     container.innerHTML = `<div id="" class="container">
     <h1>SITO IN COSTRUZIONE</h1>   
    </div>`;
+   body.setAttribute('style', 'background-color: red')
 }
